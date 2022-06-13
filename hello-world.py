39278c0e2e83359347be9e77deac2a7f967d193a
Aashutosh Shrivastav <AASHUTOSH.SHRIVASTAV@finastra.com>
2022-06-13 12:59:03 +0530

    python-practice first commit

---
+++ hello-world.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+print("Hello World");
+x = input("input some text : ");
+y="Echo";
+if(x == "Hello"):
+    print(y+" : "+"Hi");
+else:
+    print(y+" : "+x);
+
+print(locals())
+
+
